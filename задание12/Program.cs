ffe92b2b2695646361c377959b776df29054859b
Дом <@DESKTOP-7GF1H2I>
2017-09-07 22:14:06 +0500

    2

--- задание12/Program.cs
+++ задание12/Program.cs
@@ -17,7 +17,12 @@ namespace задание12
             int[] MasThree = new int[N];
             Random R = new Random ();
 
-            for (int i = 0; i < N; i++) MasOne[i] = R.Next(-50,50);
+            for (int i = 0; i < N; i++)
+            {
+                MasOne[i] = R.Next(-50, 50);
+                MasTwo[i] = MasOne[i];
+                MasThree[i] = MasOne[i];
+            }
             for (int i = 0; i < N; i++) Console.Write("{"+(i+1)+ "}" + MasOne[i]+ "  ");
             Console.WriteLine();
 
@@ -27,6 +32,12 @@ namespace задание12
             Console.WriteLine(q);
             for (int i = 0; i < N; i++) Console.Write("{" + (i + 1) + "}" + MasOne[i] + "  ");
 
+
+            MasTwo = Merge_Sort(MasTwo);
+
+            System.Console.WriteLine("\n\nThe array after sorting:");
+            foreach (Int32 x in MasTwo)
+                System.Console.Write(x + " ");
             Console.ReadKey();
         }
         public static void ArrSort(int[] array,ref int q)
@@ -63,6 +74,34 @@ namespace задание12
                 }
                 left = b;//Сохраним последнюю перестановку как границу
             }
+        }
+
+        static Int32[] Merge_Sort(Int32[] massive)
+        {
+            if (massive.Length == 1)
+                return massive;
+            Int32 mid_point = massive.Length / 2;
+            return Merge(Merge_Sort(massive.Take(mid_point).ToArray()), Merge_Sort(massive.Skip(mid_point).ToArray()));
+        }
+
+        static Int32[] Merge(Int32[] mass1, Int32[] mass2)
+        {
+            Int32 a = 0, b = 0;
+            Int32[] merged = new int[mass1.Length + mass2.Length];
+            for (Int32 i = 0; i < mass1.Length + mass2.Length; i++)
+            {
+                if (b < mass2.Length && a < mass1.Length)
+                    if (mass1[a] > mass2[b])
+                        merged[i] = mass2[b++];
+                    else //if int go for
+                        merged[i] = mass1[a++];
+                else
+                  if (b < mass2.Length)
+                    merged[i] = mass2[b++];
+                else
+                    merged[i] = mass1[a++];
+            }
+            return merged;
         }
     }
 }
